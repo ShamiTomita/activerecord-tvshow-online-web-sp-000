60b1af9e80b4c9c796b2d03a906d5756751c0c27
Shami Tomita <shamitomita@gmail.com>
2021-04-21 18:50:26 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,6 +1,6 @@
 class CreateShows < ActiveRecord::Migration[5.2]
   def change
-    create_table :tv_shows do |t|
+    create_table :shows do |t|
       t.string :name 
       t.string :day 
       t.string :network 
