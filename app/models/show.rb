8b672dd752dbeed249bcb983b2a51e3b8a28c09e
Shami Tomita <shamitomita@gmail.com>
2021-04-21 19:04:19 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,21 +1,24 @@
 class Show < ActiveRecord::Base
 
-def self.highest_rating 
-    Show.maximum(:rating)
-end 
-
-def self.most_popular_show
-  rating = self.highest_rating
-  Show.find_by(rating: rating)
-end 
+  def self.highest_rating 
+      Show.maximum(:rating)
+  end 
   
-def self.lowest_rating 
-  Show.minimum(:rating)
-end 
-  
-def self.least_popular_show 
-  rating = self.lowest_rating
-  Show.find_by(rating: rating)
-end
+  def self.most_popular_show
+    rating = self.highest_rating
+    Show.find_by(rating: rating)
+  end 
+    
+  def self.lowest_rating 
+    Show.minimum(:rating)
+  end 
+    
+  def self.least_popular_show 
+    rating = self.lowest_rating
+    Show.find_by(rating: rating)
+  end
 
+  def self.ratings_sum 
+    Show.sum(:rating)
+  end 
 end 
